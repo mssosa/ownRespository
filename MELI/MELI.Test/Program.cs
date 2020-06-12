3d6ebbcae4d06b4a26346abcf091bd7eb4b314fc
Lic. Matias Sosa <lic_mssosa@hotmail.com>
2020-06-12 19:24:15 -0300

    Testproject

---
+++ MELI/MELI.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MELI.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
